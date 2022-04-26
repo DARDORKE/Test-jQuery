1a437cfad41f8f3b47e7b98ac1c6f8c0bd6e40a5
Kévy DARDOR <kevy.dardor@gmail.com>
2022-04-26 10:40:04 +0200

    Interaction

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 $(document).ready(() => {
-    $('h1, h2, .content').css('color', 'green');
-    $('p.content:last-of-type').css('font-style', 'italic');
+    let goal = $('#goals');
+    $.data(goal, "goals", {
+      0 : "Récupérer le code CSS",
+      1 : "Modifier le CSS",
+      2 : "Parcourir une liste d'éléments",
+      3 : "Associer des données à un élément"
+    });
+
+    $('#goals li').each(function(index){
+        $(this).text($.data( goal, "#goals")[index])
+    })
 });
