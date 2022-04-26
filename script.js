78e95d49348b44cd1387603b76ec4fd788bf0a7c
Kévy DARDOR <kevy.dardor@gmail.com>
2022-04-26 10:58:11 +0200

    Ajout / Suppression

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 $(document).ready(() => {
-    let goal = $('#goals');
-    $.data(goal, "goals", {
-      0 : "Récupérer le code CSS",
-      1 : "Modifier le CSS",
-      2 : "Parcourir une liste d'éléments",
-      3 : "Associer des données à un élément"
-    });
-
-    $('#goals li').each(function(index){
-        $(this).text($.data( goal, "#goals")[index])
-    })
+   $('#introduction').remove(); //Suppression d'un élément//
+    let list = $('#goals');
+   list.prepend("<li>Objectif 0</li>"); //Ajoute un élément en début de liste goals//
+   list.append("<li>Objectif 5</li>"); //Ajoute un élément en fin de liste goals//
 });
